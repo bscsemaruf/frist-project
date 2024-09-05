fc91661c6438e21464abc8b65e05c29fa9ea7ccf
Maruf <maruf@Marufs-MacBook-Air.local>
2024-09-06 00:39:27 +0600

    ok

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send(' hi Hello World!');
 });
 
 export default app;
